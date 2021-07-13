f4daae6365a4d95518d4f61fd2d886cba2e8668e
Navya Dahiya <d.navya2312@gmail.com>
2021-07-13 12:16:28 +0530

    ui

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { v1 as uuid } from "uuid";
+// import {styles} from './design.css'
 
 const CreateRoom = (props) => {
     function create() {
@@ -8,7 +9,23 @@ const CreateRoom = (props) => {
     }
 
     return (
-        <button onClick={create}>Create room</button>
+        <button onClick={create} style={{
+            alignItems: "center", 
+            border:"none",
+            width:"380px",
+            height:"86px",
+            fontSize:"36px",
+            background:"linear-gradient(45deg, transparent 5%, #FF013C 5%)",
+            color:"#fff",
+            letterSpacing: "3px",
+            lineHeight: "88px",
+            boxShadow: "6px 0px 0px #00E6F6",
+            outline: "transparent",
+            position: "relative",
+            display: "block",
+            marginLeft: "auto",
+            marginRight: "auto"
+        }}>Create room</button>
     );
 };
 
